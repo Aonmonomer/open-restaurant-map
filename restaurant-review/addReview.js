06b3ea5d6e19e55c4d27ed253efd7206aefc6734
Seng Kit Mun <cellkit11@gmail.com>
2023-01-18 22:50:35 -0500

    Redirection between webpage

---
+++ restaurant-review/addReview.js
@@ -0,0 +1,4 @@
+document.getElementById('btnBackToIndex').onclick = function () {
+  location.href =
+    'file:///Users/sengkitmun/ga_seir/projects/open-restaurant-map/restaurant-review/index.html'
+}
