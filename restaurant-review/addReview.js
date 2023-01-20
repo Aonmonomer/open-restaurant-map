bc61d085a9f507efaa009ebac876b8c70869ab5f
Seng Kit Mun <cellkit11@gmail.com>
2023-01-19 20:29:58 -0500

    save to local storage done

--- restaurant-review/addReview.js
+++ restaurant-review/addReview.js
@@ -1,4 +1,19 @@
-document.getElementById('btnBackToIndex').onclick = function () {
+const backToIndex = () => {
   location.href =
     'file:///Users/sengkitmun/ga_seir/projects/open-restaurant-map/restaurant-review/index.html'
+}
+
+localStorage.setItem('reviews', '[]')
+
+const createReview = () => {
+  const restName = document.getElementById('restaurantName').value
+  const review = { name: restName, rating: 5 }
+  const reviews = JSON.stringify([review])
+
+  let allreviews = localStorage.getItem('reviews')
+  allreviews = JSON.parse(allreviews)
+  allreviews.push(review)
+
+  allreviews = JSON.stringify(allreviews)
+  localStorage.setItem('reviews', allreviews)
 }
