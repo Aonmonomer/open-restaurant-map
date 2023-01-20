5c64b33e2a714249a5049e09986c95baeae4d18e
Seng Kit Mun <cellkit11@gmail.com>
2023-01-19 23:06:34 -0500

    radio button to local storage done

--- restaurant-review/addReview.js
+++ restaurant-review/addReview.js
@@ -7,7 +7,23 @@ localStorage.setItem('reviews', '[]')
 
 const createReview = () => {
   const restName = document.getElementById('restaurantName').value
-  const review = { name: restName, rating: 5 }
+  let checkedRadio = 5
+
+  if (document.getElementById('1star').checked == true) {
+    checkedRadio = 1
+  } else if (document.getElementById('2star').checked == true) {
+    checkedRadio = 2
+  } else if (document.getElementById('3star').checked == true) {
+    checkedRadio = 3
+  } else if (document.getElementById('4star').checked == true) {
+    checkedRadio = 4
+  } else if (document.getElementById('5star').checked == true) {
+    checkedRadio = 5
+  } else {
+    alert('Please select a rating.')
+  }
+
+  const review = { name: restName, rating: checkedRadio }
   const reviews = JSON.stringify([review])
 
   let allreviews = localStorage.getItem('reviews')
