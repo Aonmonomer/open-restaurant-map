a421d111490889caf7277da80a16133ed870fa81
Seng Kit Mun <cellkit11@gmail.com>
2023-01-24 17:46:21 -0500

    restaurant name validation

--- restaurant-review/addReview.js
+++ restaurant-review/addReview.js
@@ -8,23 +8,28 @@ if (localStorage.getItem('reviews') == null) {
 
 const createReview = () => {
   const restName = document.getElementById('restaurantName').value
-  let checkedRating = 0
 
-  const ratingArr = document.getElementsByName('rating')
-  for (let i = 0; i < ratingArr.length; i++) {
-    if (ratingArr[i].checked == true) {
-      checkedRating = parseInt(ratingArr[i].value)
+  if (restName === '' || restName === null) {
+    alert('Please enter a restaurant name.')
+  } else {
+    let checkedRating = 0
+
+    const ratingArr = document.getElementsByName('rating')
+    for (let i = 0; i < ratingArr.length; i++) {
+      if (ratingArr[i].checked == true) {
+        checkedRating = parseInt(ratingArr[i].value)
+      }
     }
+
+    const review = { name: restName, rating: checkedRating }
+
+    let allreviews = localStorage.getItem('reviews')
+    allreviews = JSON.parse(allreviews)
+    allreviews.push(review)
+
+    allreviews = JSON.stringify(allreviews)
+    localStorage.setItem('reviews', allreviews)
+
+    backToIndex()
   }
-
-  const review = { name: restName, rating: checkedRating }
-
-  let allreviews = localStorage.getItem('reviews')
-  allreviews = JSON.parse(allreviews)
-  allreviews.push(review)
-
-  allreviews = JSON.stringify(allreviews)
-  localStorage.setItem('reviews', allreviews)
-
-  backToIndex()
 }
